80a4fbc6bb1ad71d58c1f0bafdbceb0996eab8d1
bao0303-gif <yun0322bao0303@gmail.com>
2026-02-09 20:42:33 +0800

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-export const GAME_CONFIG = {
-  images:{ tiles:["1.png","2.png","3.png","4.png","5.png"], win:"win.png"},
-  levels:[
-    {id:1,rows:7,cols:7,moves:18,targetScore:900},
-    {id:2,rows:8,cols:8,moves:16,targetScore:1500},
-    {id:3,rows:9,cols:9,moves:14,targetScore:2200}
-  ]
-};
